db468fbb99abb4a536a6f249d338ab9503529756
Costin Manolache <costin@google.com>
2017-07-25 10:30:53 -0700

    Add a small script to build on alpine/MUSL - requires a small patch to disable backtrace

---
+++ tools/build_alpine.sh
@@ -0,0 +1,15 @@
+
+# docker run -it -v /ws/istio:/workspace alpine
+
+apk update
+
+apk add  g++ gcc make cmake git bash go libnetfilter_acct-dev libnetfilter_cttimeout-dev 	libexecinfo-dev
+# For uuid dep
+apk add util-linux-dev libuuid
+
+cd /workspace
+mkdir cmake-alpine
+cd cmake-alpine
+cmake ..
+
+make
