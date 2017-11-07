0aa3e70094235ae82c4b45a0b50eb3e9e6556f7a
Costin Manolache <costin@gmail.com>
2017-11-06 16:09:09 -0800

    Helper to start docker as user

---
+++ dbuild.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Small shell script to run a transient docker build step, as current user
+# The command will be run with the ID and USER of the caller of the script,
+# using the current directory mounted as "WS".
+
+IMG=$1
+shift
+CMD=$*
+
+WS=${WS-/workspace}
+
+cd $TOP
+docker run --rm -u $(id -u) -it \
+     -v $PWD:$WS -w $WS -e USER=$USER \
+     $IMG \
+     /bin/bash -c "$CMD"
